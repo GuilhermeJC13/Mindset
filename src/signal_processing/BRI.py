7f79ba184f845212e0661b567ac961eb391b05aa
mateuslca <mateuslca2020@gmail.com>
2024-09-19 14:51:13 -0300

    processando dados da touca

---
+++ src/signal_processing/BRI.py
@@ -0,0 +1,11 @@
+from live_advance import LiveAdvance
+
+CLIENT_SECRET = 'zNmQlj2tRXg43B9oH1mjSY1mSDdggqJIEVUYNQRvjlqbRZYjNJwDrNxEwIpXiB73F5450Xy41vJj0NtCRjV7TwxKd3VsKckmPRFlzhTlsnqcFafCjY6msd3n9QtvhORU'
+CLIENT_ID = '7vPRhxTutkNjsKhZ9xn6YW6z6JJbXhYSh3NRx1YG'
+
+def main():
+    workflow = LiveAdvance(app_client_id=CLIENT_ID, app_client_secret=CLIENT_SECRET)
+    workflow.start("embarcado")
+
+if __name__ == "__main__":
+    main()
